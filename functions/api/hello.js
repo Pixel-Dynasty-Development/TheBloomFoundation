16b65e6537c1f368b0d7e772a9a383625d9ba4c9
Lunchtime52 <ryan@switchitoffice.com>
2025-10-21 10:29:11 -0400

    chore: remove unused hello.js API function

--- functions/api/hello.js
+++
@@ -1,9 +0,0 @@
-// This function handles requests to /api/hello
-export function onRequestGet() {
-	const data = { message: "Hello from your new API!" };
-
-	// Return a JSON response
-	return new Response(JSON.stringify(data), {
-		headers: { "Content-Type": "application/json" },
-	});
-}
